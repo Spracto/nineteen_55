db2a2cc364753bce4e630569c04c2003a0d26005
Barclay Iversen <barclayiversen@gmail.com>
2016-04-20 14:46:08 -0700

    re-re-init

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ app.use(bodyParser.json());
 
 require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
+
 app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response){
-	response.send("<h1> Hello Word </h1>");
-});
+// app.get('/', function(request, response){
+// 	response.send("<h1> Hello Word </h1>");
+// });
 app.use(express.static(path.join(__dirname, '/client')));
 
 app.listen(app.get('port'),  function(){
