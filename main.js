82d05935f86e8295005b9556d32a525679895de8
Barclay Iversen <barclayiversen@gmail.com>
2016-04-20 15:03:39 -0700

    trying more stuff to try and get a real project on heroku

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response){
- response.send("<h1> Hello world </h1>");
+ response.render("/client/index.html");
 });
 app.listen(app.get('port'), function(){
  console.log("listening on port ", app.get('port'));
