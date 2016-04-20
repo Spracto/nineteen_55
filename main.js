363cd2cf8740ae2d6166b5d4ab35501ae616d9ab
Barclay Iversen <barclayiversen@gmail.com>
2016-04-20 15:06:04 -0700

    spaghetti on the wall

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var app = express();
+app.use(express.static(path.join(__dirname, '/client')));
+
+require('./server/config/mongoose.js');
+require('./server/config/routes.js')(app);
+
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function(request, response){
  response.render("/client/index.html");
